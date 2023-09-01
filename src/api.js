ce3b2c7fd1f61b15872eaa47a5885dcd22e6d5f8
ShivamSingh226 <shivamsingh226@gmail.com>
2023-09-01 12:26:24 +0530

    Added weather components

---
+++ src/api.js
@@ -0,0 +1,11 @@
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5737e14fd0mshf097ec9a0d1e441p121c05jsn095a8d7d157a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
